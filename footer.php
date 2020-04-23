74fbae3efc08a01ac56b098509ba3952c23dba23
vincent sureau <vincentsureau5@gmail.com>
2020-04-23 19:25:52 +0200

    changé style footer

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		</div><!-- .container -->
 	</div><!-- #content -->
     <?php get_template_part( 'footer-widget' ); ?>
-	<footer id="colophon" class="site-footer <?php echo Vincent_Sureau_Portfolio_bg_class(); ?>" role="contentinfo">
+	<footer id="colophon" class="site-footer <?php echo Vincent_Sureau_Portfolio_bg_class(); ?> bg-duck-blue" role="contentinfo">
 		<div class="container pt-3 pb-3">
             <div class="site-info">
 				Made with <span class="font-weight-bold">&heartsuit;</span> in Toulon by <strong><?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?></strong></a>
