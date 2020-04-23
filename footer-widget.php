51772656cd82664da0873c0a7e69374adb2b3849
vincent sureau <vincentsureau5@gmail.com>
2020-04-23 19:14:56 +0200

    corrigé sticky footer

--- footer-widget.php
+++ footer-widget.php
@@ -1,17 +1,14 @@
 <?php
 
 if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) {?>
-        <div id="footer-widget" class="row m-0 <?php if(!is_theme_preset_active()){ echo 'bg-light'; } ?>">
+        <div id="footer-widget" class="row m-0 pt-3<?php if(!is_theme_preset_active()){ echo 'bg-secondary'; } ?>">
             <div class="container">
                 <div class="row">
                     <?php if ( is_active_sidebar( 'footer-1' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-1' ); ?></div>
+                        <div class="col-12 col-md-6"><?php dynamic_sidebar( 'footer-1' ); ?></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer-2' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-2' ); ?></div>
-                    <?php endif; ?>
-                    <?php if ( is_active_sidebar( 'footer-3' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-3' ); ?></div>
+                        <div class="col-12 col-md-6"><?php dynamic_sidebar( 'footer-2' ); ?></div>
                     <?php endif; ?>
                 </div>
             </div>
