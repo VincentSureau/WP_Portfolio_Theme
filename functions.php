3ef39247fb7845ef65e3586350ff58fc4057a0af
vincent sureau <vincentsureau5@gmail.com>
2020-04-23 17:03:48 +0200

    remove content editor from front-age

--- functions.php
+++ functions.php
@@ -78,30 +78,6 @@ function Vincent_Sureau_Portfolio_setup() {
 endif;
 add_action( 'after_setup_theme', 'Vincent_Sureau_Portfolio_setup' );
 
-
-/**
- * Add Welcome message to dashboard
- */
-function Vincent_Sureau_Portfolio_reminder(){
-        $theme_page_url = 'https://afterimagedesigns.com/vincent-sureau-portfolio/?dashboard=1';
-
-            if(!get_option( 'triggered_welcomet')){
-                $message = sprintf(__( 'Welcome to Vincent Sureau Portfolio Theme! Before diving in to your new theme, please visit the <a style="color: #fff; font-weight: bold;" href="%1$s" target="_blank">theme\'s</a> page for access to dozens of tips and in-depth tutorials.', 'vincent-sureau-portfolio' ),
-                    esc_url( $theme_page_url )
-                );
-
-                printf(
-                    '<div class="notice is-dismissible" style="background-color: #6C2EB9; color: #fff; border-left: none;">
-                        <p>%1$s</p>
-                    </div>',
-                    $message
-                );
-                add_option( 'triggered_welcomet', '1', '', 'yes' );
-            }
-
-}
-add_action( 'admin_notices', 'Vincent_Sureau_Portfolio_reminder' );
-
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
